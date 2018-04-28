22053916d30b3f858fa8f3a393e1ecc96bdf8c81
MarinichRoman <rmarinich@bk.ru>
2018-04-28 19:26:36 +0300

    1.3

--- app/src/main/java/com/romarinichgmail/predreliz/RegistrationActivity.java
+++ app/src/main/java/com/romarinichgmail/predreliz/RegistrationActivity.java
@@ -16,10 +16,13 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
 
-    private EditText mEmail, mPassword;
-    private Button mLogin, mRegistration;
+public class RegistrationActivity extends AppCompatActivity {
+
+    private EditText mEmail, mPassword,mFio,mPhone,mConfirmPassword;
+    private Button  mRegistration;
 
     private FirebaseAuth mAuth;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -27,7 +30,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_registration);
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -36,7 +39,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 if(user!=null){
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    Intent intent = new Intent(RegistrationActivity.this, MainActivity.class);
                     startActivity(intent);
                     finish();
                     return;
@@ -45,37 +48,44 @@ public class LoginActivity extends AppCompatActivity {
         };
 
         mEmail = (EditText) findViewById(R.id.email);
+        mFio = (EditText) findViewById(R.id.name);
+        mPhone = (EditText) findViewById(R.id.phone);
         mPassword = (EditText) findViewById(R.id.password);
+        mConfirmPassword = (EditText) findViewById(R.id.confirm_password);
 
-        mLogin = (Button) findViewById(R.id.login);
+
         mRegistration = (Button) findViewById(R.id.registration);
 
         mRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this,RegistrationActivity.class);
-                startActivity(intent);
-                finish();
-                return;
-            }
-        });
-
-        mLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
                 final String email = mEmail.getText().toString();
                 final String password = mPassword.getText().toString();
-                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(RegistrationActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(!task.isSuccessful()){
-                            Toast.makeText(LoginActivity.this, "sign in error", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(RegistrationActivity.this, "sign up error", Toast.LENGTH_SHORT).show();
+                        }else{
+                            String fio = mFio.getText().toString();
+                            String emaill = mEmail.getText().toString();
+                            String phone = mPhone.getText().toString();
+                                                        String user_id = mAuth.getCurrentUser().getUid();
+                            Map<String, Object> data = new HashMap<>();
+                            data.put("email", emaill);
+                            data.put("name", fio);
+                            data.put("phone", phone);
+                            db.collection("driver").document(user_id).set(data);
+
+                            //DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(user_id);
+                            //current_user_db.setValue(true);
                         }
                     }
                 });
-
             }
         });
+
+
     }
 
 
@@ -90,4 +100,3 @@ public class LoginActivity extends AppCompatActivity {
         mAuth.removeAuthStateListener(firebaseAuthListener);
     }
 }
-
