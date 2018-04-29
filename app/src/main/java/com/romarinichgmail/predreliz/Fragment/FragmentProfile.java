c39826df9cb92cea52dfd4c280a45837958ba49a
MarinichRoman <rmarinich@bk.ru>
2018-04-29 11:36:30 +0300

    1.4 profile, no image

--- app/src/main/java/com/romarinichgmail/predreliz/Fragment/FragmentProfile.java
+++ app/src/main/java/com/romarinichgmail/predreliz/Fragment/FragmentProfile.java
@@ -2,17 +2,41 @@ package com.romarinichgmail.predreliz.Fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.romarinichgmail.predreliz.R;
 
 
-public class FragmentProfile extends Fragment {
 
+public class FragmentProfile extends Fragment{
 
+    private TextView pName;
+    private TextView pNum_order;
+    private TextView pEmail;
+    private TextView pAddress;
+    private TextView pRating;
+    private TextView pPhone;
+
+    private String mProfileImageUrl;
+
+    private ImageView mProfileImage;
+
+    private String userID;
+
+    private FirebaseAuth mAuth;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
     public FragmentProfile() {
 
     }
@@ -23,7 +47,63 @@ public class FragmentProfile extends Fragment {
                              Bundle savedInstanceState) {
 
         v = inflater.inflate(R.layout.fragment_profile, container, false);
+        pEmail = (TextView) v.findViewById(R.id.pEmail);
+        pName = (TextView) v.findViewById(R.id.pName);
+        pNum_order = (TextView) v.findViewById(R.id.pNum_order);
+        pAddress = (TextView) v.findViewById(R.id.pAddress);
+        pRating = (TextView) v.findViewById(R.id.pRating);
+        pPhone = (TextView) v.findViewById(R.id.pPhone);
+        mAuth = FirebaseAuth.getInstance();
+        userID = mAuth.getCurrentUser().getUid();
+        loadDataFormFirebase();
+
+        mProfileImage = (ImageView) v.findViewById(R.id.profileImage);
+
         return v;
+
+
     }
+
+
+
+    public void loadDataFormFirebase() {
+
+
+        db.collection("driver").document(userID);
+
+        DocumentReference infoDriver= db.collection("driver").document(userID);
+        infoDriver.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot doc = task.getResult();
+                    StringBuilder dataAddres = new StringBuilder("");
+                    dataAddres.append(doc.getString("location"));
+                    pAddress.setText(dataAddres.toString());
+
+                    StringBuilder dataEmail = new StringBuilder("");
+                    dataEmail.append(doc.getString("email"));
+                    pEmail.setText(dataEmail.toString());//
+
+                    StringBuilder dataName = new StringBuilder("");
+                    dataName.append(doc.getString("name"));
+                    pName.setText(dataName.toString());
+
+                    StringBuilder dataPhone = new StringBuilder("");
+                    dataPhone.append(doc.getString("phone"));
+                    pPhone.setText(dataPhone.toString());
+
+                    StringBuilder dataRating = new StringBuilder("");
+                    dataRating.append(doc.getString("rating"));
+                    pRating.setText(dataRating.toString());//
+
+                    StringBuilder dataNum_poezdok = new StringBuilder("");
+                    dataNum_poezdok.append(doc.getString("num_poezdok"));
+                    pNum_order.setText(dataNum_poezdok.toString());
+                }
+            }
+        });
+    }
+
 
 }
